93ee185f8c8ce5978ed1577d80a8c5577d051704
Dan Udey <dan@athinkingape.com>
2011-08-16 16:17:57 -0700

    Added Summify to filtered sites

--- Disbarred.safariextension/disbarred.js
+++ Disbarred.safariextension/disbarred.js
@@ -2,6 +2,7 @@ var stuffs = {
         'visibli.com': de_visibli,
         'stumbleupon.com': de_stumbl,
         'reddit.com': de_reddit,
+        'summify.com': de_summify,
 };
 
 function de_reddit()
@@ -9,6 +10,14 @@ function de_reddit()
     if (document.location.href.indexOf('reddit.com/toolbar/inner') > 0)
     {
         top.location.href = document.getElementsByTagName('frame')[1].src;
+    }
+}
+
+function de_summify()
+{
+    if (document.location.href.indexOf('summify.com/story/') > 0)
+    {
+        document.location.href = document.getElementById('summiFrame').src;
     }
 }
 
